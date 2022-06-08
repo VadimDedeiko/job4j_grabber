019a7f1ffbc928bcfb10434c4444d4b40cf5bba5
Windows <ivinjc@gmail.com>
2022-06-08 13:51:46 +0300

    Новое сÐJob c параметрами [#260360 #310378]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -5,18 +5,23 @@ import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
-import static org.quartz.JobBuilder.*;
-import static org.quartz.TriggerBuilder.*;
-import static org.quartz.SimpleScheduleBuilder.*;
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
 
 public class AlertRabbit {
     public static void main(String[] args) {
         try {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
+            JobDataMap dataMap = new JobDataMap();
+            dataMap.put("connect", getConnection());
+            JobDetail job = newJob(Rabbit.class).usingJobData(dataMap).build();
             int interval = Integer.parseInt(getProperties().getProperty("rabbit.interval"));
             SimpleScheduleBuilder times = simpleSchedule()
                     .withIntervalInSeconds(interval)
@@ -26,19 +31,33 @@ public class AlertRabbit {
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
+            Thread.sleep(10000);
+            scheduler.shutdown();
+        } catch (SchedulerException | IOException | InterruptedException
+                 | SQLException | ClassNotFoundException se) {
             se.printStackTrace();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
+    }
+
+    private static Connection getConnection() throws IOException, SQLException, ClassNotFoundException {
+        Class.forName(getProperties().getProperty("jdbc.driver"));
+        return DriverManager.getConnection(
+                getProperties().getProperty("jdbc.url"),
+                getProperties().getProperty("jdbc.username"),
+                getProperties().getProperty("jdbc.password"));
     }
 
     public static class Rabbit implements Job {
         @Override
         public void execute(JobExecutionContext context) {
+            try {
+                getConnection()
+                        .prepareStatement("insert into rabbit(created_date) values ('2020-12-01');")
+                        .executeUpdate();
+            } catch (SQLException | IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
             System.out.println("Rabbit runs here ...");
-
-
         }
     }
 
