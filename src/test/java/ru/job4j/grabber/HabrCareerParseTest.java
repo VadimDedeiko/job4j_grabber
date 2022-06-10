a4f4528513ef5c8a60d6e8ad1bc1dfe281dce98d
Windows <ivinjc@gmail.com>
2022-06-10 10:13:52 +0300

    Парсинг https://www.sql.ru/forum/job-offers/3 [#285210 #310688]

---
+++ src/test/java/ru/job4j/grabber/HabrCareerParseTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.grabber;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class HabrCareerParseTest {
+
+
+/*    @Test*/
+/*    public void whenretrieveDescription() throws IOException {
+        HabrCareerParse habrCareerParse = new HabrCareerParse();
+        String str = habrCareerParse.retrieveDescription("https://career.habr.com/vacancies/1000101645");
+
+    }*/
+}
