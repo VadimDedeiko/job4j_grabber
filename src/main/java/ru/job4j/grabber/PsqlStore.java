3860a862d3fe5a7abddba77a2ea2249e075a6723
Windows <ivinjc@gmail.com>
2022-06-10 19:57:14 +0300

    PsqlStore [#285209 #311038]

--- src/main/java/ru/job4j/grabber/PsqlStore.java
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -1,6 +1,8 @@
 package ru.job4j.grabber;
 
 import ru.job4j.grabber.model.Post;
+import ru.job4j.grabber.utils.DateTimeParser;
+import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,6 +14,18 @@ import java.util.Properties;
 public class PsqlStore implements Store, AutoCloseable {
 
     private Connection cnn;
+
+    public static void main(String[] args) throws Exception {
+        PsqlStore psqlStore = new PsqlStore(new Properties());
+        DateTimeParser dateTimeParser = new HabrCareerDateTimeParser();
+        HabrCareerParse parse = new HabrCareerParse(dateTimeParser);
+        List<Post> list = parse.list("https://career.habr.com//vacancies/java_developer");
+        list.forEach(psqlStore::save);
+        List<Post> listGetAll = psqlStore.getAll();
+        listGetAll.forEach(System.out::println);
+        System.out.println(psqlStore.findById(3));
+        psqlStore.close();
+    }
 
     public PsqlStore(Properties cfg) {
         try (InputStream in = PsqlStore.class.getClassLoader().getResourceAsStream("app.properties")) {
@@ -27,14 +41,14 @@ public class PsqlStore implements Store, AutoCloseable {
                     cfg.getProperty("jdbc.password")
             );
         } catch (Exception e) {
-            throw new IllegalStateException(e);
+            e.printStackTrace();
         }
     }
 
     @Override
     public void save(Post post) {
         try (PreparedStatement ps = cnn.prepareStatement(
-                "insert into table post(name,text,link,created) values (?,?,?,?);"
+                "insert into post(name,description,link,created) values (?,?,?,?);"
         )) {
             ps.setString(1, post.getTitle());
             ps.setString(2, post.getDescription());
@@ -68,7 +82,7 @@ public class PsqlStore implements Store, AutoCloseable {
     public Post findById(int id) {
         Post post = null;
         try (PreparedStatement ps =
-                     cnn.prepareStatement("select * from items where id=?;")) {
+                     cnn.prepareStatement("select * from post where id=?;")) {
             ps.setInt(1, id);
             ResultSet resultSet = ps.executeQuery();
             if (resultSet.next()) {
@@ -90,7 +104,7 @@ public class PsqlStore implements Store, AutoCloseable {
     private Post setPost(ResultSet resultSet) throws SQLException {
         return new Post(resultSet.getInt("id"),
                 resultSet.getString("name"),
-                resultSet.getString("text"),
+                resultSet.getString("description"),
                 resultSet.getString("link"),
                 resultSet.getTimestamp("created").toLocalDateTime()
         );
